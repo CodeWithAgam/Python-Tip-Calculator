f01354aeeecfac2678f9a68646bac74043e4bf59
Agamdeep Singh <agamdeep579@gmail.com>
2021-12-10 11:22:17 +0530

    Added the code

---
+++ main.py
@@ -0,0 +1,23 @@
+# The below function splits the bill between people and also Calculates the tip
+# Created by Agamdeep Singh / CodeWithAgam
+# Youtube: CodeWithAgam
+# Github: CodeWithAgam
+# Instagram: @agamdeep_21, @coderagam001
+# Twitter: @CoderAgam001
+# Linkdin: Agamdeep Singh
+
+# Print a Welcome message
+print("Welcome To Python Tip Calculator!")
+
+# Get the inputs from the user
+bill = float(input("What was the total bill? "))
+tip = int(input("How much tip should be given? 10, 12, or 15? "))
+people = int(input("Number of people to split the bill? "))
+
+# Calculate the bill with the tip and bill per person
+bill_with_tip = (tip / 100 * bill + bill)
+bill_per_person = (bill_with_tip / people)
+
+# Round off the amount to 2 digits and print the vaule
+final_amount = round(bill_per_person, 2)
+print(f"Each person should pay: {final_amount}")
